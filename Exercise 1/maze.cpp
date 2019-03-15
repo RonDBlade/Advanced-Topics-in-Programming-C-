bb61b96c7cee660f99bee8bf25f69862fe5fbf87
roypel <31568986+roypel@users.noreply.github.com>
2019-03-15 16:29:36 +0200

    Skeleton for maze functions

--- Exercise 1/maze.cpp
+++ Exercise 1/maze.cpp
@@ -3,11 +3,13 @@
 #include <string>
 #include <cstring>
 
+
 #define delimiter " \t\r\n"
 #define HEADER_LENGTH 3
 #define TOKEN_LENGTH 2
 
 using std::cout
+using std::string
 
 bool check_if_int(char* token){
     for (int i = 0; i < token.length(); i++){
@@ -22,20 +24,46 @@ bool is_header_valid(ifstream input_file, int* maze_data){
     string line;
     char* token;
     getline(input_file, line);
-    for (int i = 0; i < HEADER_LENGTH; i++){
+    while (i < HEADER_LENGTH){
         getline(input_file, line);
         token = strtok(line, delimiter);
-        if (i == 0){
-            if (token != "MaxSteps")
+        switch(i){
+        case 0:
+            if (token != "MaxSteps"){
+                // Print error
+            }
+            break;
+        case 1:
+            if (token != "Rows"){
+                // Print error
+            }
+            break;
+        case 2:
+            if (token != "Cols"){
+                // Print error
+            }
+            break;
         }
-        for (int j = 0; j < TOKEN_LENGTH; j++){
-
-            token = strtok(NULL, delimiter);
+        token = strtok(nullptr, delimiter);
+        if (token != "="){
+            // Print error
         }
+        token = strtok(nullptr, delimiter);
         if (check_if_int(token)){
-
+                maze_data[i] = stoi(token)
+        }
+        else{
+            // Print error
+        }
+        if (token != nullptr){
+            // Print error - more tokens than needed
         }
     }
+}
+
+bool file_exists(const string& file_path){
+    ifstream file(file_path.c_str());
+    return file.good();
 }
 
 int main(int argc, char* argv[]){
@@ -46,4 +74,8 @@ int main(int argc, char* argv[]){
     if !(is_header_valid(input_file)){
         // Errors
     }
+    if (file_exists(argv[2])){
+        // Print errors
+    }
+    parse_maze();
 }
