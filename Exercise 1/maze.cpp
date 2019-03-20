ec5c977a6e1d17980a48d13a59035a50792ddc26
roypel <31568986+roypel@users.noreply.github.com>
2019-03-20 21:06:13 +0200

    Update maze.cpp

--- Exercise 1/maze.cpp
+++ Exercise 1/maze.cpp
@@ -1,14 +1,32 @@
+#include <vector>
+#include <fstream>
+#include <string>
 
+using std::vector;
+using std::string;
 
 class MazeInfo{
     int maxSteps;
     int rows;
     int cols;
-    std::vector<char> mazeData;
+    vector<vector<char>> mazeData;
 
 public:
     MazeInfo(int maxSteps_, int rows_, int cols_): maxSteps(maxSteps_), rows(rows_), cols(cols_){
-        mazeData.resize(_rows * _cols);
+        mazeData.resize(rows);
+        for(int i = 0; i < rows; i++){
+            mazeData[i].resize(cols);
+        }
     }
-    parse_maze()
+    parse_maze(ifstream input_file){
+        int curr_col, curr_row = 0;
+        string line;
+        while (curr_row < rows){
+            line = getline(input_file)
+            curr_col = 0;
+            while (curr_col < cols) && (cur_col < line.length()){
+
+            }
+        }
+    }
 }maze;
