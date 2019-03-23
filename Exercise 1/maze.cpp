847c38fa97481f496a8f77ca8dea55fd9d7407b9
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2019-03-23 17:33:04 +0200

    Update maze.cpp

--- Exercise 1/maze.cpp
+++ Exercise 1/maze.cpp
@@ -1,3 +1,4 @@
+#include <maze.h>
 #include <vector>
 #include <fstream>
 #include <string>
@@ -5,28 +6,19 @@
 using std::vector;
 using std::string;
 
-class MazeInfo{
-    int maxSteps;
-    int rows;
-    int cols;
-    vector<vector<char>> mazeData;
-
-public:
-    MazeInfo(int maxSteps_, int rows_, int cols_): maxSteps(maxSteps_), rows(rows_), cols(cols_){
-        mazeData.resize(rows);
-        for(int i = 0; i < rows; i++){
-            mazeData[i].resize(cols);
+MazeInfo::MazeInfo(int maxSteps_, int rows_, int cols_): maxSteps(maxSteps_), rows(rows_), cols(cols_){
+    mazeData.resize(rows);
+    for(int i = 0; i < rows; i++){
+        mazeData[i].resize(cols);
+    }
+}
+MazeInfo::parse_maze(ifstream input_file){
+    int curr_col, curr_row = 0;
+    string line;
+    while (curr_row < rows){
+        line = getline(input_file)
+        curr_col = 0;
+        while (curr_col < cols) && (cur_col < line.length()){
         }
     }
-    parse_maze(ifstream input_file){
-        int curr_col, curr_row = 0;
-        string line;
-        while (curr_row < rows){
-            line = getline(input_file)
-            curr_col = 0;
-            while (curr_col < cols) && (cur_col < line.length()){
-
-            }
-        }
-    }
-}maze;
+}
