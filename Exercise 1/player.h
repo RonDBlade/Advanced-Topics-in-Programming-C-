ec0e4945fd2a20f612bcaed22dd2b3bad29ea3e9
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2019-03-29 17:30:03 +0300

    Update player.h

--- Exercise 1/player.h
+++ Exercise 1/player.h
@@ -1,25 +1,29 @@
 #ifndef PLAYER_H_INCLUDED
 #define PLAYER_H_INCLUDED
-#include <vector>
+#include <map>
 #include <utility>
 
 #define STARTING_MAP_SIZE 5 /*why?*/
 
-using std::vector;
+using std::map;
 using std::pair;
 
 enum class Move{LEFT, RIGHT, UP, DOWN, BOOKMARK};
 
 class Player{
-    vector<vector<int> > player_map; /*should change to map of maps.From my understanding of the implementation we start the player from position
+    map<int, map<int,char> > player_map; /*should change to map of maps.From my understanding of the implementation we start the player from position
     (0,0) in his perspective,and if we treat some positions as "-" positions(down from (0,0) is (0,-1)),we cant do that with vectors*/
     pair<int, int> current_position;
     pair<int, int> bookmark_position; /*should keep this as game manager data,since he doesnt remember where he put it*/
+    void <int, int> hitWall(){
+        
+    }
 
 public:
     Player();
     pair<int, int> player_pos()const;
-    Move move();
+    Move chooseMove();
+    void update_map(char to_put,Move where);
 };
 
 #endif // PLAYER_H_INCLUDED
