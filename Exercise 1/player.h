b3b905ba771ccd72e60f53b3bbda12606402c985
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2019-04-01 22:58:23 +0300

    Update player.h

--- Exercise 1/player.h
+++ Exercise 1/player.h
@@ -3,7 +3,6 @@
 #include <map>
 #include <utility>
 
-#define STARTING_MAP_SIZE 5 /*why?*/
 
 using std::map;
 using std::pair;
@@ -13,6 +12,8 @@ enum class Move{LEFT, RIGHT, UP, DOWN, BOOKMARK};
 class Player{
     map<int, map<int,char> > player_map; /*should change to map of maps.From my understanding of the implementation we start the player from position
     (0,0) in his perspective,and if we treat some positions as "-" positions(down from (0,0) is (0,-1)),we cant do that with vectors*/
+    map<int, map<int,int> > when_wasOn; //keeps for each location when we have last visited it.helps us decide which way to go.
+    int moveNumber=0;//keeps the count of moves for
     pair<int, int> current_position;
     pair<int, int> bookmark_position; /*should keep this as game manager data,since he doesnt remember where he put it*/
     void hitWall(){}
@@ -21,16 +22,19 @@ class Player{
             return false;
         if(!player_map[x].count(y))//this specific point hasn't been discovered yet
             return false;
-        return true;
+        return true;//we have visited this cell in the past
     }
     bool isWall(int x,int y){
-    return player_map[x][y]=='#';
+        return player_map[x][y]=='#';
     }
 public:
     Player();
     pair<int, int> player_pos()const;
     Move move();
     void updateMap(char to_put,Move where);
+    void increaseMovenum();
+    void setLocMove(int x,int y);
+    int getLocMove(int x, int y);
 };
 
 #endif // PLAYER_H_INCLUDED
