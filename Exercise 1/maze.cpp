c80cabd60423793c7ca03ff74ec5c3de8ba7c990
roypel <31568986+roypel@users.noreply.github.com>
2019-04-03 19:52:07 +0300

    Changed from resize to reserve again

--- Exercise 1/maze.cpp
+++ Exercise 1/maze.cpp
@@ -1,10 +1,7 @@
 #include "maze.h"
 
 Maze::Maze(unsigned int maxSteps_, unsigned int rows_, unsigned int cols_): maxSteps(maxSteps_), rows(rows_), cols(cols_), mazeValid(true){
-    mazeData.resize(rows);
-    for(unsigned int i = 0; i < rows; i++){
-        mazeData[i].reserve(cols);
-    }
+    mazeData.reserve(rows);
 }
 
 bool Maze::parse_maze(ifstream &input_file){
