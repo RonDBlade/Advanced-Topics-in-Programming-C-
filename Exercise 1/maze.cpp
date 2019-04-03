adc482306886dd45d3176276be170c1f0c2f96f0
roypel <31568986+roypel@users.noreply.github.com>
2019-04-03 19:40:07 +0300

    Fixed reserve to resize

--- Exercise 1/maze.cpp
+++ Exercise 1/maze.cpp
@@ -1,7 +1,7 @@
 #include "maze.h"
 
 Maze::Maze(unsigned int maxSteps_, unsigned int rows_, unsigned int cols_): maxSteps(maxSteps_), rows(rows_), cols(cols_), mazeValid(true){
-    mazeData.reserve(rows);
+    mazeData.resize(rows);
     for(unsigned int i = 0; i < rows; i++){
         mazeData[i].reserve(cols);
     }
