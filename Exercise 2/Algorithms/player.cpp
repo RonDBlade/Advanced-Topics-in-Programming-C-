63b2dae6929449f41c05513a2ea4551c49ef3512
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2019-05-11 11:35:11 +0300

    Update player.cpp

--- Exercise 2/Algorithms/player.cpp
+++ Exercise 2/Algorithms/player.cpp
@@ -91,61 +91,63 @@ pair<int, int> Player::bookmark_pos()const{
     return bookmark_position;
 }
 
-void Player::hitBookmark(){//we want to update the map of locations
-    int cnt=moveNumber;
-    bookmark_on=false;
-    put_bookmark/=2;
-    if(put_bookmark<10)
-        put_bookmark=10;
-    bookmark_count=0;
-    current_position=bookmark_pos();
-    pair<int, int> pos=bookmark_pos();
-    pair<Move,char> tmp;
-    unsigned int tmpsize=movekeep.size();//so wont need to keep calculating this
-    for(int i=0;(unsigned int)i<tmpsize;i++){
-        cnt--;
-        if((unsigned int)i==tmpsize-1){//last iteration.this is where we started.which means it has to be a space.
-            continue;
+void Player::hitBookmark(int seq){//we want to update the map of locations
+        if(seq==highest_bookmark){
+            int cnt=moveNumber;
+            bookmark_on=false;
+            put_bookmark/=2;
+            if(put_bookmark<10)
+                put_bookmark=10;
+            bookmark_count=0;
+            current_position=bookmark_pos();
+            pair<int, int> pos=bookmark_pos();
+            pair<Move,char> tmp;
+            unsigned int tmpsize=movekeep.size();//so wont need to keep calculating this
+            for(int i=0;(unsigned int)i<tmpsize;i++){
+                cnt--;
+                if((unsigned int)i==tmpsize-1){//last iteration.this is where we started.which means it has to be a space.
+                    continue;
+                }
+                tmp=movekeep[tmpsize-1-i];
+                if(tmp.first==Move::BOOKMARK)
+                    continue;
+                if(tmp.second==' '){
+                    if(tmp.first==Move::UP){
+                        pos.second--; //put it in each if SPECIFICALLY AND ON PURPOSE.DONT PUT THIS IN THE END TO SAVE LINES
+                    }
+                    else if(tmp.first==Move::DOWN){
+                        pos.second++;
+                    }
+                    else if(tmp.first==Move::LEFT){
+                        pos.first++;
+                    }
+                    else if(tmp.first==Move::RIGHT){
+                        pos.first--;
+                    }
+                    player_map[pos.first][pos.second]=tmp.second;
+                    when_wasOn[pos.first][pos.second]=cnt;
+                }
+                else{//if we walked into a wall in that one,then only need to set it as a wall for later.
+                    if(tmp.first==Move::UP){
+                        player_map[pos.first][pos.second+1]=tmp.second;
+
+                    }
+                    else if(tmp.first==Move::DOWN){
+                        player_map[pos.first][pos.second-1]=tmp.second;
+
+                    }
+                    else if(tmp.first==Move::LEFT){
+                        player_map[pos.first-1][pos.second]=tmp.second;
+
+                    }
+                    else if(tmp.first==Move::RIGHT){
+                        player_map[pos.first+1][pos.second]=tmp.second;
+
+                    }
+                }
+            }
+            movekeep.clear();
         }
-        tmp=movekeep[tmpsize-1-i];
-        if(tmp.first==Move::BOOKMARK)
-            continue;
-        if(tmp.second==' '){
-            if(tmp.first==Move::UP){
-                pos.second--; //put it in each if SPECIFICALLY AND ON PURPOSE.DONT PUT THIS IN THE END TO SAVE LINES
-            }
-            else if(tmp.first==Move::DOWN){
-                pos.second++;
-            }
-            else if(tmp.first==Move::LEFT){
-                pos.first++;
-            }
-            else if(tmp.first==Move::RIGHT){
-                pos.first--;
-            }
-            player_map[pos.first][pos.second]=tmp.second;
-            when_wasOn[pos.first][pos.second]=cnt;
-        }
-        else{//if we walked into a wall in that one,then only need to set it as a wall for later.
-            if(tmp.first==Move::UP){
-                player_map[pos.first][pos.second+1]=tmp.second;
-
-            }
-            else if(tmp.first==Move::DOWN){
-                player_map[pos.first][pos.second-1]=tmp.second;
-
-            }
-            else if(tmp.first==Move::LEFT){
-                player_map[pos.first-1][pos.second]=tmp.second;
-
-            }
-            else if(tmp.first==Move::RIGHT){
-                player_map[pos.first+1][pos.second]=tmp.second;
-
-            }
-        }
-    }
-    movekeep.clear();
 }
 
 Player::Player(): current_position(0, 0), bookmark_position(0, 0){
