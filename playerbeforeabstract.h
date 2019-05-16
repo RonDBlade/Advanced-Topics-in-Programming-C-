d2426932e7a922ac9ad117dde0fd8359f6a3c80a
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2019-05-16 21:43:21 +0300

    Update playerbeforeabstract.h

--- playerbeforeabstract.h
+++ playerbeforeabstract.h
@@ -4,7 +4,6 @@
 #include <utility>
 #include <vector>
 
-
 using std::map;
 using std::pair;
 using std::vector;
@@ -12,82 +11,42 @@ using std::vector;
 enum class Move{LEFT, RIGHT, UP, DOWN, BOOKMARK};
 
 class Player{
-    map<int, map<int,char> > player_map; /*should change to map of maps.From my understanding of the implementation we start the player from position
-    (0,0) in his perspective,and if we treat some positions as "-" positions(down from (0,0) is (0,-1)),we cant do that with vectors*/
+    map<int, map<int,char> > player_map;
     map<int, map<int,int> > when_wasOn; //keeps for each location when we have last visited it.helps us decide which way to go.
     int moveNumber=0;//keeps the count of moves for
-    Move lastMove;
-    int bookmark_counter; //counts how many bookmarks we have set,so that when it is 0 we know we can set bookmark_on to false.
+    Move lastMove;//keeps the last move the player did,for restoring after we hit a wall
     pair<int, int> current_position;
-    int max_bookmark=0;//tells us which bookmark is the highest we met,so that if we meet an older one,we won't redo the data we learned from it.
-    vector<pair<pair<int, int>, int > > bookmark_position; //queue of pairs of pairs.the first pair is the x,y coordinations,and then the solo number is bookmark number
-    int offset=0; //offset of which move we need to sub in the information about the bookmark position that we hit.
-    vector<pair<vector<pair<Move,char> >,int> > movekeep;//keeps us the moves we did until we hit a bookmark.the int is the number of bookmark of the vector.
+    vector<pair<int, int> > bookmark_position; /*should keep this as game manager data,since he doesnt remember where he put it*/
+    vector<vector<pair<Move,char> > >movekeep;//keeps us the moves we did until we hit a bookmark
     //IMPORTANT: to add the moves to this vector,we cant do it in move.this adds to the vector what move we did and were it got us to.so it has to be push_back()ed in gameManager
+    int total_bookmarks=0; //the total amount of bookmarks we put down
+    int biggest_met=0;//largest bookmark sequence number we already stepped on,so we can ignore younger ones.
     int put_bookmark=10;//when bookmark_count==put_bookmark then we put a new bookmark
-    int bookmark_count=0;//for when to put bookmarks calculations
+    int bookmark_count=0;
     bool bookmark_on=false;//true when we have a bookmark on the board
     bool moved_to_new=false;
-    bool isKnown(int x,int y){//this PRIVATE method checks if player has been on this coordination yet.On HIS map,not the maze map.
-        if(!player_map.count(x))//no point in this x coordination has been discovered,so this one hasn't either
-            return false;
-        if(!player_map[x].count(y))//this specific point hasn't been discovered yet
-            return false;
-        return true;//we have visited this cell in the past
-    }
-    bool isWall(int x,int y){
-        return player_map[x][y]=='#';
-    }
-
-    bool putBookmark(){
-        if(moveNumber==1){
-            //just started the game
-            bookmark_on=true;
-            return true;
-        }
-        else if(bookmark_on==false&&moved_to_new){
-            //if we dont have a bookmark and this is a new place we just went to
-            bookmark_on=true;
-            return true;
-        }
-        else if(bookmark_count==put_bookmark){//if we waited too long for a bookmark
-            put_bookmark*=2;
-            bookmark_on=true;
-            return true;
-        }
-        return false;
-    }
-
-    void pushtoMoveKeep(Move moved,char tileWentTo){
-    movekeep.back().push_back({moved,tileWentTo});
-}
-
-    int findMoveNum(Move mov){
-        if(mov==Move::UP)
-            return Player::getLocMove(current_position.first,current_position.second+1);
-        if(mov==Move::DOWN)
-            return Player::getLocMove(current_position.first,current_position.second-1);
-        if(mov==Move::LEFT)
-            return Player::getLocMove(current_position.first-1,current_position.second);
-        return Player::getLocMove(current_position.first+1,current_position.second);
-    }
 
 public:
     Player();
-    pair<int, int> player_pos()const;
-    pair<int, int> bookmark_pos()const;
     Move move();
-    void updateMap(char to_put,Move where);
-    void increaseMovenum();
-    void setLocMove(int x,int y);
-    int getLocMove(int x, int y);
-    void hitBookmark(int seq);
-    int firstPlyr();
-    int secondPlyr();
-    int firstBook();
-    int secondBook();
-    void setbyMove(Move mov);
     void hitWall();
+    void hitBookmark(int seq);
+
+
+private:
+    pair<int, int> player_pos()const;//return player position
+    pair<int, int> bookmark_pos(int seq)const;//return bookmark position
+    void updateMap(char to_put,Move where);
+    void setLocMove(int x,int y);//sets the move of the location given
+    int getLocMove(int x, int y);
+    void setbyMove(Move mov);//set data on player map
+    int findMoveNum(Move mov);//for a square around the player in the mov direction,checks when we last visited it
+    void pushtoMoveKeep(Move moved,char tileWentTo);
+    bool putBookmark();//checks if we should put a bookmark in the player position
+    bool isWall(int x,int y);//checks if player already knows this is a wall or it isnt a wall
+    bool isKnown(int x,int y);//checks if player has already been here
+    void removeBookmarkUntill(int seq);//removes bookmarks until the bookmark we met,which is given at seq
+    void cleanKeepUntil(int seq);//removes data from move_keep until the bookmark we met,which is given at seq
 };
 
-#endif // PLAYER_H_INCLUDED
+#endif // _314615600_A_H_INCLUDED
