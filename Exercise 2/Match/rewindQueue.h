2c699310589e049e519a665ce76d8eef73463b4d
roypel <31568986+roypel@users.noreply.github.com>
2019-05-27 16:25:34 +0300

    Runs like a boss

--- Exercise 2/Match/rewindQueue.h
+++ Exercise 2/Match/rewindQueue.h
@@ -9,12 +9,11 @@ class RewindQueue {
 
     std::vector<T> vec;
     typename std::vector<T>::iterator it;
+    bool initalized;
     std::mutex m;
 
 public:
-    RewindQueue() {
-        it = vec.begin();
-    }
+    RewindQueue(): initalized(false){}
 
     void setSize(int expectedSize){
         vec.reserve(expectedSize);
@@ -27,11 +26,17 @@ public:
     }
 
     bool popElement(T& elem) {
+        if (!initalized){
+            it = vec.begin();
+            initalized = true;
+        }
         std::lock_guard<std::mutex> lock(m);
         std::cout << "Trying to get out an element" << endl;
         if(it == vec.end()) {
+            std::cout << "No elements" << endl;
             return false;
         }
+        std::cout << "Got an element, number " << it-vec.begin() << " out of " << vec.size() << endl; \
         elem = *it;
         it++;
         return true;
