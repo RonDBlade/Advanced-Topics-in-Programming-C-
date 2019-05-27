739e62611204d7961c0435d72e3f24d619fa155e
roypel <31568986+roypel@users.noreply.github.com>
2019-05-27 16:16:31 +0300

    Removed unnecessary pointer

--- Exercise 2/Match/main.cpp
+++ Exercise 2/Match/main.cpp
@@ -1,7 +1,7 @@
 #include "matchManager.h"
 
 int main(int argc, char *argv[]){
-    std::shared_ptr<matchManager> mM = std::make_shared<matchManager>();
-    mM->processMatch(argc, argv);
+    matchManager mM = matchManager();
+    mM.processMatch(argc, argv);
     return 0;
 }
